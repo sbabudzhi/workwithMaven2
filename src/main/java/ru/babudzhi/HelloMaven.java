87eb2ddb7c74ccffcb001c642d8c2f91742db256
babudzhi <babudhi@lanit.ru>
2018-04-20 10:28:23 +0300

    with convention

---
+++ src/main/java/ru/babudzhi/HelloMaven.java
@@ -0,0 +1,17 @@
+package ru.babudzhi;
+
+/**
+ * Класс для учебной работы с Maven
+ *
+ * @author Babudzhi
+ */
+public class HelloMaven {
+
+    /**
+     * Главный метод
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hey");
+    }
+}
--- src/main/java/ru/babudzhi/mavHello.java
+++
@@ -1,6 +0,0 @@
-package ru.babudzhi;
-public class mavHello {
-    public static void main(String[] args) {
-        System.out.println("Hey");
-    }
-}
